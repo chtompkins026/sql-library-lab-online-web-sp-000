366ae2068462f2d4cefd4e945c722efa471d632a
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-08 15:20:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUE ()  
 
-id INTEGER PRIMARY KEY,
-  title TEXT,
-  author_id INTEGER,
-  subgenre_id INTEGER
+-- id INTEGER PRIMARY KEY,
+--   title TEXT,
+--   author_id INTEGER,
+--   subgenre_id INTEGER
