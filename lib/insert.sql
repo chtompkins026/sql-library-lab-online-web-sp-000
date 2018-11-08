fd83b3a9634b0abe93ddb3d3cba05d9c20c01f0f
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-08 15:40:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,4 +9,4 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "Book 1", 2000, 1), (2
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Name1", "Motto1", "species1", 1, 1), (2, "Name2", "Motto2", "species2", 1, 1), (3, "Name3", "Motto3", "species1", 1, 1), (4, "Name4", "Motto4", "species1", 1, 1), (5, "Name5", "Motto6", "species2", 2, 2), (6, "Motto66", "Motto7", "species1", 2, 2), (7, "Name69", "Motto77", "species1", 2, 2), (8, "Name779", "Motto66", "species1", 2, 2);
 
-
+INSERT INTO subgenres (id, name) VALUES (1, "subgenre1"), (2, "subgenre2");
