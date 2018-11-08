519af49b0fdadadcedac0aadb0a6a2e4d14d7a3c
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-08 15:26:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,12 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Lord of the R
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Book 1", 2000, 1), (2, "Book 2", 1990, 1), (3, "Book 3", 2010, 2), (4, "Book 4", 2005, 2), (5, "Book 66", 2013, 2), (6, "Book 89", 2015, 1);
 
 INSERT INTO 
+
+
+-- characters (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   motto TEXT,
+--   species TEXT,
+--   author_id INTEGER,
+--   series_id INTEGER
