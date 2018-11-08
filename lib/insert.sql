4cddd5bbd25ff2e2e11869f2e5ac9cdc32e0185c
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-08 15:23:38 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1,"Lord of the Rings",)  
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Lord of the Rings", 1, 1), (2, "Lord of My Undies", 2, 2); 
 
 -- id INTEGER PRIMARY KEY,
 --   title TEXT,
