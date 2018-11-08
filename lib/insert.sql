70c19d52202a4a30e352c98fa8e950b64e234b1b
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-08 17:08:08 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Lord of the Rings", 1, 1), (2, "Lord of My Undies", 2, 2); 
+
+-- id INTEGER PRIMARY KEY,
+--   title TEXT,
+--   author_id INTEGER,
+--   subgenre_id INTEGER
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Book 1", 2000, 1), (2, "Book 2", 1990, 1), (3, "Book 3", 2010, 2), (4, "Book 4", 2005, 2), (5, "Book 66", 2013, 2), (6, "Book 89", 2015, 1);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Name1", "Motto1", "species1", 1, 1), (2, "Name2", "Motto2", "species2", 1, 1), (3, "Name3", "Motto3", "species1", 1, 1), (4, "Name4", "Motto4", "species1", 1, 1), (5, "Name5", "Motto6", "species2", 2, 2), (6, "Motto66", "Motto7", "species1", 2, 2), (7, "Name69", "Motto77", "species1", 2, 2), (8, "Name779", "Motto66", "species1", 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES (1, "subgenre1"), (2, "subgenre2");
+
+INSERT INTO authors (id, name) VALUES (1, "authorname1"), (2, "authorname2");
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 1), (6, 2, 1), (7, 3, 1), (8, 1, 2);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 1), (10, 4, 1), (11, 5, 1), (12, 6, 1), (13, 4, 1), (14, 5, 2), (15, 3, 2), (16, 4, 2);
